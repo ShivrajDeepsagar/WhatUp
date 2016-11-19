8465754917814bb99a4a7d4615ff72c3dd838857
Shivraj <shivaraj.deepasagar@gmail.com>
2016-11-19 11:04:28 +0530

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :routines
+  root to: "routines#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
